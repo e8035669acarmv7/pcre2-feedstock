d797e23655df613aec11878c4f82506ee8eb07ef
Filipe <ocefpaf@gmail.com>
2018-11-13 22:20:02 -0200

    remove .la (#19)

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,9 @@ CXXFLAGS="${CXXFLAGS} -O3"
     --prefix="${PREFIX}" \
     --enable-jit \
 
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
+
+# We can remove this when we start using the new conda-build.
+find $PREFIX -name '*.la' -delete
